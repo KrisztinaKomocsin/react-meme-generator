e0a136587cd46b33d4ec92936b28fa9c2218a303
Krisztina Komocsin <krisztina.komocsin@gmail.com>
2022-05-12 17:17:25 +0200

    Add updated version

--- src/App.js
+++ src/App.js
@@ -2,26 +2,59 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
-  useEffect(() => {
-    fetch('https://api.memegen.link/templates')
-      .then((response) => response.json())
-      .then((json) => console.log(json));
-  }, []);
+  const [url, setUrl] = useState(
+    'https://api.memegen.link/images/aag/foo/bar.png',
+  );
+  const [topText, settopText] = useState('');
+  const [bottomText, setbottomText] = useState('');
+  const [inputName, setInputName] = useState('');
 
   return (
     <div className="App">
       <h1>RANDOM MEME GENERATOR!</h1>
-      <label>
-        Top Text
+
+      <div className="memeTemplate">
+        <label>
+          Meme templates
+          <input
+            className="memeTemplate"
+            onChange={(event) => {
+              setInputName(event.currentTarget.value);
+            }}
+            value={inputName}
+          />
+        </label>
+      </div>
+      <img src="https://api.memegen.link/images/aag/foo/bar.png" alt="" />
+      <div className="inputFields">
+        <input
+          onChange={(event) => {
+            setInputName(event.currentTarget.value);
+          }}
+          value={inputName}
+          type="text"
+          id="topText"
+          placeholder="Top"
+          value={''}
+        />
         <br />
-        <input type="text" />
-      </label>
-      <br />
-      <label>
-        Bottom Text
-        <br />
-        <input type="text" />
-      </label>
+        <input
+          onChange={(event) => {
+            setInputName(event.currentTarget.value);
+          }}
+          value={inputName}
+          type="text"
+          id="bottomText"
+          placeholder="Bottom"
+          value={''}
+        />
+      </div>
+
+      <form className="memeButtons">
+        <button>Generate</button>
+        <button>Download</button>
+        <button>Clear</button>
+      </form>
     </div>
   );
 }
